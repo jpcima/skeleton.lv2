34a454b47b8c8e292860aa5b98e2ec606559f65c
JP Cimalando <jpcima@noreply.users.github.com>
2017-05-28 17:43:51 +0200

    add -Wl,--no-undefined to the Linux build

--- cmake/LV2Project.cmake
+++ cmake/LV2Project.cmake
@@ -44,6 +44,10 @@ macro(add_lv2_fx name)
     LIBRARY_OUTPUT_DIRECTORY "lv2/${PROJECT_NAME}.lv2"
     C_VISIBILITY_PRESET "hidden"
     CXX_VISIBILITY_PRESET "hidden")
+  if(CMAKE_SYSTEM_NAME MATCHES "Linux")
+    set_property(TARGET ${name} APPEND_STRING PROPERTY
+      LINK_FLAGS " -Wl,--no-undefined")
+  endif()
   if(USE_DYN_MANIFEST)
     file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/lv2/${PROJECT_NAME}.lv2/manifest.ttl"
       "@prefix lv2:  <http://lv2plug.in/ns/lv2core#> .
@@ -77,6 +81,10 @@ macro(add_lv2_ui name)
     LIBRARY_OUTPUT_DIRECTORY "lv2/${PROJECT_NAME}.lv2"
     C_VISIBILITY_PRESET "hidden"
     CXX_VISIBILITY_PRESET "hidden")
+  if(CMAKE_SYSTEM_NAME MATCHES "Linux")
+    set_property(TARGET ${name} APPEND_STRING PROPERTY
+      LINK_FLAGS " -Wl,--no-undefined")
+  endif()
   target_include_directories(${name}
     PRIVATE ${LV2_INCLUDE_DIRS}
     PRIVATE ${Boost_INCLUDE_DIRS})
